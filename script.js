ccf534097cdc9b8df45f768e47becdcf45cea06e
Dmitriy <devilins24@gmail.com>
2022-03-18 08:21:25 +0200

    add gsap animation

---
+++ script.js
@@ -0,0 +1,7 @@
+let timeline = gsap.timeline({ defaults: { duration: 1, opacity: 0, ease: "power2.in" } });
+
+timeline
+  .from(".illustration", { scale: 0, duration: 0.5, ease: "back" })
+  .from(".text-section>*", { stagger: 0.4 })
+  .from(".attribution", { duration: 0.5, y: "10px" })
+  .from(".message-box>*", { scale: 0, stagger: 1 }, 2);
